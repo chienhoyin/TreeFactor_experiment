03036b0e7babf33046edd76327eb872a369ca10f
chienhoyin <77372981+chienhoyin@users.noreply.github.com>
2024-07-31 20:59:28 -0400

    Revert "Update state.h, add prior regressor"
    This reverts commit db1eba9f7c7558346102f1066d6cb2ad30d79965.

--- src/state.h
+++ src/state.h
@@ -14,7 +14,6 @@ public:
     arma::mat *F; // for Bayes tree
     arma::mat *regressor; // for Bayes tree
     arma::mat *H; // placeholder
-    arma::mat *prior_regressor; //
     arma::vec *weight;
     arma::vec *loss_weight;
     arma::vec *stocks; // pointer to the index of stocks, same number of rows as X
@@ -68,7 +67,6 @@ public:
         this->R = &R;
         this->Z = &Z;
         this->H = &H;
-        this->prior_regressor = &prior_regressor;
         this->weight = &portfolio_weight;
         this->loss_weight = &loss_weight;
         this->stocks = &stocks;
